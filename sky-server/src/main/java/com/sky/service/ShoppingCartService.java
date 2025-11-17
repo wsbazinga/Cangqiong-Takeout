de84ac8ffe618372a5d0e35fa163b8e44b009d5f
wsbazinga <1114870372@qq.com>
2025-11-17 19:09:57 +0800

    用户下单功能

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -19,4 +19,10 @@ public interface ShoppingCartService {
      * 清空购物车
      */
     void cleanShoppingCard();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
